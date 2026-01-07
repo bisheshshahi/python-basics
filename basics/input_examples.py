34deb92073cbb53b8178c7719ca282c8d7839b11
Bishesh Shahi <bisheshshahi14@gmail.com>
2026-01-07 15:06:39 +0545

    add input_examples.py in basics folder

---
+++ basics/input_examples.py
@@ -0,0 +1,13 @@
+# print("Bishesh Shahi")
+
+#Taking input from user
+name = input("Enter your name:")
+age = int(input("Enter your age"))
+print("Your name is ", name)
+print("Your age is ", age)
+
+#Taking integer, float input from user
+num1 = int(input("Enter one integer:"))
+print("The integer number you've entered is ", num1)
+
+#For float add float instead of int
