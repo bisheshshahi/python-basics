ba56202262132f8884e5cf3a6d80e03930664f67
Bishesh Shahi <bisheshshahi14@gmail.com>
2026-01-12 22:08:27 +0545

    stop tracking this file

--- basics/string_slicing.py
+++
@@ -1,8 +0,0 @@
-name = "Bishesh"
-print(name[0:4])#prints from 0th index up to 3rd index, ignores 4th index value
-print(name[:4])#having value 0 or not no changes 
-print(name[:])#prints whole length
-print(len(name))#len function prints the number of character 
-print(name[0:-3])#this prints up to total length -3
-print(name[-4:-2])#7 is the total character in my name, 7-4=3:7-2=5,3:5
-print(name[len(name)-4:len(name)-2])
