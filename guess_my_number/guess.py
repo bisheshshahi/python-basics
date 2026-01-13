f86112b38b43d47374c8df8bc9ca6d64089b30db
Bishesh Shahi <bisheshshahi14@gmail.com>
2026-01-13 12:41:16 +0545

    use try except instead of just if else

--- guess_my_number/guess.py
+++ guess_my_number/guess.py
@@ -1,19 +1,17 @@
 import random
-print("Guess the number game")
+
 num = random.randrange(1,101)
 print(num)
-user_num = int(input("Guess the number(integer):"))
 
-while(user_num!=num):
-  # user_num = int(input("Guess the number:"))
-  if user_num == num:
-    print("Congratulations you guessed the correct number.")
-  elif user_num<num:
-    print("The number is smaller.")
-    user_num = int(input("Guess the number:"))
-  elif user_num>num:
-    print("The number is bigger.")
-    user_num = int(input("Guess the number:"))
-  else:
-    print("Please enter one positive integer")
-    user_num = int(input("Guess the number:"))    
+while True:
+  try:
+    user_num = int(input("Enter a number(integer):"))
+    if user_num<num:
+      print("The number is smaller.")
+    elif user_num>num:
+      print("The number is bigger.")
+    else:
+      print("Congratulations you guessed the number correctly.")
+      break
+  except Exception:
+    print("Enter numbers only")
