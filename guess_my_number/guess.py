736c3046adf484e385787cfcb3c176172ac34dbb
Bishesh Shahi <bisheshshahi14@gmail.com>
2026-01-13 14:10:47 +0545

    add number of tries of user

--- guess_my_number/guess.py
+++ guess_my_number/guess.py
@@ -2,16 +2,18 @@ import random
 
 num = random.randrange(1,101)
 print(num)
-
+count = 0
 while True:
   try:
     user_num = int(input("Enter a number(integer):"))
+    count= count+1
     if user_num<num:
       print("The number is smaller.")
     elif user_num>num:
       print("The number is bigger.")
     else:
       print("Congratulations you guessed the number correctly.")
+      print("You guessed the number in",count,"tries.")
       break
   except Exception:
     print("Enter numbers only")
