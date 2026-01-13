18a4aa0d467926a4695d49841a823df620fadac1
Bishesh Shahi <bisheshshahi14@gmail.com>
2026-01-13 19:47:47 +0545

    add range check for user and add f strings

--- guess_my_number/guess.py
+++ guess_my_number/guess.py
@@ -5,18 +5,23 @@ num = random.randrange(1,101)
 print(num)
 count = 0
 
+print("Guess the number between 1 and 100")
+
 #loop
 while True:
   try:
     user_num = int(input("Enter a number(integer):"))
+    if not (user_num>=1 and user_num<=100):
+      print("Please enter a number between 1 to 100.")
+      continue
     count = count+1     #no. of attempts
     if user_num<num:
-      print("The number is smaller.\n","No. of attempts=",count)
+      print(f"The secret number is smaller than this.\nNo. of attempts={count}")
     elif user_num>num:
-      print("The number is bigger.\n","No. of attempts=",count)
+      print(f"The secret number is bigger than this.\nNo. of attempts={count}")
     else:
       print("Congratulations you guessed the number correctly.")
-      print("You guessed the number in",count,"tries.")
+      print(f"You guessed the number in {count} tries.")
       break
   except Exception:
     print("Enter numbers only")
