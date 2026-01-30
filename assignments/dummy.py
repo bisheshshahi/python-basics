99e482debb0d17fabfee7afc0f476ae696734d01
Bishesh Shahi <bisheshshahi14@gmail.com>
2026-01-30 10:46:34 +0545

    add assignments folder

---
+++ assignments/dummy.py
@@ -0,0 +1,17 @@
+for i in range(1,5):
+  for j in range(1,i+1):
+    print(j,end="")
+  print()
+
+
+'''
+*
+**
+***
+****
+'''
+
+for i in range(1,5):
+  print()
+  for j in range(1,i+1):
+    print('*',end="")
