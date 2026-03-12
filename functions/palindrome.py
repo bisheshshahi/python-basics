db889d75d00f197e6554e415484d646c38a28207
Bishesh Shahi <bisheshshahi14@gmail.com>
2026-03-12 21:45:36 +0545

    add lower function for words

--- functions/palindrome.py
+++ functions/palindrome.py
@@ -1,16 +1,18 @@
 #Palin as palindrome
 
 def palin(check,original):
+  check = check.lower()
   if len(check)<=1:
     print(f"The given text '{original}' is palindrome.")
   else:
     if check[0] == check[-1]:
       palin(check[1:-1],original)
     else:
-      print(f"The given text '{original}' is not a palindrome.")
+      print(f"The given word '{original}' is not a palindrome.")
 
 while True:
   text = input("Enter the word to check whether the word is palindrome or not (or type 'exit' to quit): ")
   if text.lower().strip() == "exit":
     break
+
   palin(text,text)
