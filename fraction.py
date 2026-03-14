d64960cc2340b4250bcbfafffa00a76d88f534f3
Bishesh Shahi <bisheshshahi14@gmail.com>
2026-03-14 17:43:11 +0545

    create datatype fraction

---
+++ fraction.py
@@ -0,0 +1,46 @@
+#Creating data type 'fraction'
+
+import math #for gcd(Greatest Common Divisor)
+class Fraction:
+  def __init__(self,n,d): #n as numerator, d as denominator
+    self.num = n
+    self.den = d
+  
+  def __str__(self):
+    return f"{self.num}/{self.den}"
+    # return "{}/{}".format(self.num,self.den) Another way of using string formatting
+  
+  def __add__(self, other): #other is needed as we're doing for binary operator
+    temp_num = self.num * other.den + other.num * self.den
+    temp_den = self.den * other.den
+    return self.simplify(temp_num,temp_den)
+
+  def __sub__(self,other):
+    temp_num = self.num * other.den - other.num * self.den
+    temp_den = self.den * other.den
+    # return f"{temp_num}/{temp_den}"
+    return self.simplify(temp_num,temp_den)
+  
+  def __mul__(self, other):
+    temp_num = self.num * other.num
+    temp_den = self.den * other.den
+    return self.simplify(temp_num,temp_den)  
+  
+  def __truediv__(self, other):
+    temp_num = self.num * other.den
+    temp_den = self.den * other.num
+    return self.simplify(temp_num,temp_den)
+  
+  def simplify(self,n,d):
+    gcd = math.gcd(n,d)
+    return Fraction(n // gcd, d // gcd)
+  
+frac1 = Fraction(10,20)
+frac2 = Fraction(30,40)
+
+print(frac1)
+print(frac2)
+print(frac1+frac2)
+print(frac1-frac2)
+print(frac1*frac2)
+print(frac1/frac2)
