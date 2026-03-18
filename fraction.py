cf55521dadd681330040e582ff46e270cd7d1e3f
Bishesh Shahi <bisheshshahi14@gmail.com>
2026-03-18 10:02:15 +0545

    add _convert method to handle integer inputs in operations

--- fraction.py
+++ fraction.py
@@ -30,37 +30,63 @@ class Fraction:
   def __repr__(self):
       return f"Fraction({self.num}, {self.den})"
   
+  #Simplify
+  def simplify(self):
+    gcd = math.gcd(self.num,self.den)
+    return Fraction(self.num // gcd, self.den // gcd)
+
+  #Convert int into fraction (if needed)
+  # Internal helper method (underscore means for internal use)
+  # Ensures 'other' is a Fraction (converts int to Fraction if needed)
+  def _convert(self, other):
+    if isinstance(other,int):
+      return Fraction(other,1)
+    if isinstance(other,Fraction):
+      return other
+    raise TypeError("Unsupported type")
+  
+  #Arithmetic operations
   def __add__(self, other): #other is needed as we're doing for binary operator
+    other = self._convert(other)
     temp_num = self.num * other.den + other.num * self.den
     temp_den = self.den * other.den
-    return self.simplify(temp_num,temp_den)
+    return Fraction(temp_num,temp_den).simplify()
 
   def __sub__(self,other):
+    other = self._convert(other)
     temp_num = self.num * other.den - other.num * self.den
     temp_den = self.den * other.den
     # return f"{temp_num}/{temp_den}"
-    return self.simplify(temp_num,temp_den)
+    return  Fraction(temp_num,temp_den).simplify()
   
   def __mul__(self, other):
+    other = self._convert(other)
     temp_num = self.num * other.num
     temp_den = self.den * other.den
-    return self.simplify(temp_num,temp_den)  
+    return  Fraction(temp_num,temp_den).simplify() 
   
   def __truediv__(self, other):
+    other = self._convert(other)
     temp_num = self.num * other.den
     temp_den = self.den * other.num
-    return self.simplify(temp_num,temp_den)
+    return  Fraction(temp_num,temp_den).simplify()
+
   
-  def simplify(self,n,d):
-    gcd = math.gcd(n,d)
-    return Fraction(n // gcd, d // gcd)
-  
+#Testing
 frac1 = Fraction(10,-2)
-frac2 = Fraction(30,40)
+frac2 = Fraction(30,3)
 
 print(frac1)
 print(frac2)
+
+print("Addition")
 print(frac1+frac2)
+print("Substraction")
 print(frac1-frac2)
+print("Multiplication")
 print(frac1*frac2)
+print("Division")
 print(frac1/frac2)
+
+print("Example for _convert method")
+print(frac1 * 2)
