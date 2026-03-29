be083fb5522c6752dd37844afd7751375fe6bf11
Bishesh Shahi <bisheshshahi14@gmail.com>
2026-03-29 19:05:19 +0545

    add input validation

--- 100 problems/4. sum_of_digits.py
+++ 100 problems/4. sum_of_digits.py
@@ -2,15 +2,19 @@
 
 num = int(input("Enter 3 digit number: "))
 
-last_digit = num%10
+if len(str(num))<4 and len(str(num))>2:
+  last_digit = num%10
 
-num = num//10
+  num = num//10
 
-first_digit = num//10
-print("First digit is",first_digit)
+  first_digit = num//10
+  print("First digit is",first_digit)
 
-sec_digit = num%10
-print("Second digit is",sec_digit)
-print("Last digit is", last_digit)
+  sec_digit = num%10
+  print("Second digit is",sec_digit)
+  print("Last digit is", last_digit)
 
-print(f"The sum of 3 digits is",first_digit+sec_digit+last_digit)
+  print(f"The sum of 3 digits is",first_digit+sec_digit+last_digit)
+
+else:
+  print("Wrong input")
